adcda8e398c529f33b364a40eb1a2f4983710e9e
Jackson Souza Santos <jacksonsantos.tec@gmail.com>
2022-09-24 16:22:35 -0300

    exercício em grupo game

--- Fundamentos/seção-4-introducao-a-javaScript-e-Logica-de-programacao/aula 4.3/exer3.js
+++ Fundamentos/seção-4-introducao-a-javaScript-e-Logica-de-programacao/aula 4.3/exer3.js
@@ -1,13 +1,39 @@
-// 3 - Desenvolva um algoritmo que simule o jogo "pedra, papel e tesoura" levando em consideração que são apenas duas pessoas jogando. Imprima o resultado no formato:
+// 3 - Desenvolva um algoritmo que simule o jogo "pedra, papel e tesoura" 
+// levando em consideração que são apenas duas pessoas jogando. Imprima o resultado no formato:
 // "Player 1 won" ou "A Ties" ou "Player 2 won".
-let jogador1 = (pedra, papel, tesoura);
-let jogador2 = (pedra, papel, tesoura);
 
-if( jogador1 == pedra){
-    if(jogador2 == tesoura)
-    console.log("pedra quebra tesoura");
-}else if(jogador2 == papel){
-    console.log("papel embrulha a pedra");
-}else if(jogador2 == pedra){
-    console.log("Empate")
+
+let toten = ['pedra', 'papel','tesoura'];
+
+let player1 = toten[Math.floor(Math.random() * 2) + 1];                         //O Math.floor()método arredonda um número PARA BAIXO para o inteiro mais próximo.
+let player2 = toten[Math.floor(Math.random() * 2) + 1];                        //O Math.random()método retorna um número aleatório de 0 (inclusive) até, mas não incluindo, 1 (exclusivo).
+                                                                              //Math.floor((Math.random() * 2) + 1) retornar jogadas aleatótias entre 0 e 2
+
+if(player1 === toten[0]){
+    if(player2 === toten[1]){
+        console.log("pedra quebra tesoura, player1 wins");
+    }else if(player2 === toten [2]){
+        console.log("papel cobre pedra, player2 wins");
+    }else{
+        console.log("draw");
+    }
+}else if(player1 === toten[1]){
+    if(player2 === toten[0]){
+        console.log("pedra quebra tesoura, player2 wins");
+    }else if(player2 === toten [2]){
+        console.log("tesoura corta papel, player1 wins");
+    }else{
+        console.log("draw");
+    } 
+}else if(player1 === toten[2]){
+    if(player2 === toten[0]){
+        console.log("papel cobre pedra, player1 wins");
+    }else if(player2 === toten [1]){
+        console.log("tesoura corta papel, player2 wins");
+    }else{
+        console.log("draw");
+    } 
 }
+
+console.log(`player1${player1}`, `player2${player2}`);
+//console.log(["player1", player1].join(' '), ["player2", player2].join(' ')); //contatenando commétodo join.
