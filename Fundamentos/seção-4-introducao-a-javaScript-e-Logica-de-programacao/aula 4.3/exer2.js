168d8d7b9ec0c13380e243f5d6030a29b74c7323
Jackson Souza Santos <jacksonsantos.tec@gmail.com>
2022-09-24 16:51:49 -0300

    exercicios 3 e 4

--- Fundamentos/seção-4-introducao-a-javaScript-e-Logica-de-programacao/aula 4.3/exer2.js
+++ Fundamentos/seção-4-introducao-a-javaScript-e-Logica-de-programacao/aula 4.3/exer2.js
@@ -1,3 +1,26 @@
-// 2 - Desenvolva um algoritmo que conte quantos números, em um intervalo entre dois números, são divisíveis por 3. Exemplo: 1 e 20.
+// 2 - Desenvolva um algoritmo que conte quantos números, em um intervalo entre dois números,
+// são divisíveis por 3. Exemplo: 1 e 20.
 // Caso a quantidade seja igual a 50, exiba uma mensagem secreta (pode inventar uma). 
-// Não conta pra ninguém, mas no intervalo entre 2 e 150 existem 50 números divisíveis por 3 ;)
+// Não conta pra ninguém, mas no intervalo entre 2 e 150 existem 50 números divisíveis por 3 ;
+
+
+let intervalo = 2;
+let range = 150;
+let divisiveis = 0;
+
+for (let index = 0; intervalo <= range.length; index += 1) {
+    if (intervalo % 3 === 0) {
+        divisiveis += 1;
+    }
+    intervalo += 1;
+}
+console.log(divisiveis);
+
+ //const idade1 = 80;
+ //const verificaIdade1 = idade1 === '80'; //resultado esperado false
+ //console.log(verificaIdade1);
+//
+ //const idade2 = 26;
+//
+ //const verificaIdade2 = idade2 == '26'; //Resultado esperado true
+ //console.log(verificaIdade2);
--- Fundamentos/seção-4-introducao-a-javaScript-e-Logica-de-programacao/aula 4.3/exer4.js
+++ Fundamentos/seção-4-introducao-a-javaScript-e-Logica-de-programacao/aula 4.3/exer4.js
@@ -1,3 +1,13 @@
 // 4 - Desenvolva um algoritmo que verifica se a pessoa é maior ou menor de idade.
 // Imprima no console seguindo o exemplo: "A pessoa é maior de idade".
 // PS. Considere que uma pessoa com 18 anos ou mais é uma pessoa maior de idade.
+
+const idade = 15;
+
+const verificaIdade = idade;
+
+if(idade === 18){
+console.log("A pessoa é maior de idade");
+}else{
+    console.log("Pessoa menor de idade");
+}
