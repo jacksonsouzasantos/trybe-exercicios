a4ff41cbad50c434a100adc72878a819bee1e570
Jackson Souza Santos <jacksonsantos.tec@gmail.com>
2022-11-12 15:17:03 -0300

    Adicionando folha de estilo

--- Front-End/1 - Introdução-ao-frontend-JavasScript-assinc/ambiente-de-desenvolvimento/Super-Heros/main.js
+++ Front-End/1 - Introdução-ao-frontend-JavasScript-assinc/ambiente-de-desenvolvimento/Super-Heros/main.js
@@ -1,12 +1,13 @@
 /**importar biblioteca */
 import Swal from "sweetalert2";
+import('./style.css');
 
 /**Criando constantes para armazenar imagen,name,btn */
 
 const img = document.querySelector('#image');
 const name = document.querySelector('#name');
 const button = document.querySelector('#button');
- 
+
 /**Inserindo token de acesso */
 
 const BASE_URL = `https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id`;
@@ -20,24 +21,24 @@ const randomId = () => Math.floor(Math.random() * MAX_HEROES);
 /**Adicionar evento ao botão de click */
 button.addEventListener('click', (event) => {
 
-/** event.preventDefault() para evitar que ele recarregue*/ 
-  event.preventDefault();
+    /** event.preventDefault() para evitar que ele recarregue*/
+    event.preventDefault();
 
-  const id = randomId();
+    const id = randomId();
 
-/**fetch com a url gerada pelo id com a função randonId */
-fetch(`${BASE_URL}/${id}.json`)
-.then((result) => result.json()) /**extraindo obj com o .json */
-.then((data) => {/**recuperando informaçoẽs extraídas*/
-img.src = data.images.lg;
-name.innerHTML = data.name;
-})
+    /**fetch com a url gerada pelo id com a função randonId */
+    fetch(`${BASE_URL}/${id}.json`)
+        .then((result) => result.json()) /**extraindo obj com o .json */
+        .then((data) => {/**recuperando informaçoẽs extraídas*/
+            img.src = data.images.lg;
+            name.innerHTML = data.name;
+        })
 
-/**tratando erro com .cat() */
-.catch((error) => Swal.fire({
-    title: 'Hero not found',
-    text: error.message,
-    icon: 'error',
-    confirmButtonText: 'Cool',
-}));
+        /**tratando erro com .cat() */
+        .catch((error) => Swal.fire({
+            title: 'Hero not found',
+            text: error.message,
+            icon: 'error',
+            confirmButtonText: 'Cool',
+        }));
 });
